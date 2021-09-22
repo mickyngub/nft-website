be25ebda5a3009182d01f1602ef681beee03dd77
mickyngub <57980116+mickyngub@users.noreply.github.com>
2021-09-23 01:15:35 +0700

    Added new cat gif / text+space between section

--- src/PageContent/Content1/Content1.tsx
+++ src/PageContent/Content1/Content1.tsx
@@ -1,9 +1,20 @@
 import roadmapCat from "assets/roadmap.jpeg";
 import upcomingCat from "assets/upcoming.jpeg";
+import upcomingCatGif from "assets/upcomingCatGif.gif";
+
 const Content = () => {
   return (
-    <div className="bg-red-500">
+    <div>
+      <p className="text-7xl text-center h-32 mt-16 text-blue-900">Roadmap</p>
       <img className="w-screen h-auto" src={roadmapCat} alt="roadmapCat" />
+      <p className="text-7xl text-center h-32 mt-16 text-blue-900">
+        Upcoming...
+      </p>
+      <img
+        className="w-screen h-auto mx-auto"
+        src={upcomingCatGif}
+        alt="upcomingCatGit"
+      />
       <img
         className="w-screen h-auto mx-auto"
         src={upcomingCat}
